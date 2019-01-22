3c172baf015a96ea3be8981dbd269c09268cd015
Diogo Costa <dioogo.costa@gmail.com>
2019-01-21 23:09:54 -0200

    Seed Project

---
+++ src/main/java/com/groupsix/mapFood/MapFoodApiApp.java
@@ -0,0 +1,12 @@
+package com.groupsix.mapFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFoodApiApp {
+
+	public static void main (String[] args) {
+		SpringApplication.run(MapFoodApiApp.class, args);
+}
+}
